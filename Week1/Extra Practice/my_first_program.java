ec00f8d046ed584b1fb1b5d0c771cdf843bfb03e
Ethan-Guenther <ethan.guenther@gmail.com>
2022-04-05 16:18:20 -0500

    My First Program

---
+++ Week1/Extra Practice/my_first_program.java
@@ -0,0 +1,9 @@
+public class my_first_program
+{
+    public static void main(String[] args)
+    {
+        System.out.println("My name is Ethan Guenther");
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown in Sioux Falls, South Dakota");
+    }
+}
