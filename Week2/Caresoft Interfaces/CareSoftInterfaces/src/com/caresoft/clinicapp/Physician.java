d3368ace66bdc21b14ee5c6fb86017b9282f050b
Ethan-Guenther <ethan.guenther@gmail.com>
2022-04-16 15:10:42 -0500

    commit

---
+++ Week2/Caresoft Interfaces/CareSoftInterfaces/src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,49 @@
+package com.caresoft.clinicapp;
+
+public class Physician extends User implements HIPAACompliantUser
+{
+//... imports class definition...
+    
+    // Inside class:    	
+    // TO DO: Constructor that takes an ID
+    // TO DO: Implement HIPAACompliantUser!
+	
+    public Physician(Integer id) {
+		super(id);
+	}
+    @Override
+    public boolean assignPin(int pin)
+    {
+    	if(pin <= 999 || pin >= 10000)
+    	{
+    		return false;
+    	}
+    	this.pin = pin;
+    	return true;
+    }
+    @Override
+    public boolean accessAuthorized(Integer confirmedAuthID)
+    {
+    	if(this.id == confirmedAuthID)
+    	{
+    		return true;
+    	}
+    	return false;
+    }
+
+	public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+    
+    
+	
+    // TO DO: Setters & Getters
+	
+
+}
